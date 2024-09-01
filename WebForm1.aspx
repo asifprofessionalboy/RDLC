951d9fd630a24882bfec46b6bcd57775539f6c2c
Asif Akhtar <146723391+asifprofessionalboy@users.noreply.github.com>
2024-09-01 13:07:21 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Student.aspx.cs" Inherits="ASPNET_RDLC.Student" %>
+<%@ Register assembly="Microsoft.ReportViewer.WebForms" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Student Report</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Student Report</h1>
+        </div>
+
+        <asp:ScriptManager ID="ScriptManager1" runat="server" />
+
+        <rsweb:ReportViewer ID="ReportViewer1" runat="server" Width="100%" Height="600px">
+            <LocalReport ReportPath="StudentRPT.rdlc">
+            </LocalReport>
+        </rsweb:ReportViewer>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WebForms;
+
+namespace ASPNET_RDLC
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Load the report data and set it to the ReportViewer
+                LoadReport();
+            }
+        }
+
+        private void LoadReport()
+        {
+            // Get the data from the database
+            DataTable studentData = GetStudentData();
+
+            // Set the Report Path
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("StudentRPT.rdlc");
+
+            // Clear any existing data sources
+            ReportViewer1.LocalReport.DataSources.Clear();
+
+            // Create a new ReportDataSource with the name of your dataset in the RDLC
+            ReportDataSource rds = new ReportDataSource("DataSet1", studentData);
+
+            // Add the data source to the report
+            ReportViewer1.LocalReport.DataSources.Add(rds);
+
+            // Refresh the report
+            ReportViewer1.LocalReport.Refresh();
+        }
+
+        private DataTable GetStudentData()
+        {
+            // Define the query
+            string query = "SELECT * FROM StudentTbl";
+
+            // Create a DataTable to hold the data
+            DataTable dt = new DataTable();
+
+            // Use ADO.NET to fetch data from the database
+            using (SqlConnection con = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        // Fill the DataTable with data from the database
+                        sda.Fill(dt);
+                    }
+                }
+            }
+
+            return dt;
+        }
+    }
+}
